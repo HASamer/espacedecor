65c3bfb018b4cb80d3b780289910de678b254de7
HASamer <samerhadjali@gmail.com>
2025-09-05 00:34:48 +0100

    3rd try

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -2,83 +2,87 @@ import Image from "next/image";
 
 export default function ProductCard() {
   return (
-    <a href="#" className="block rounded-lg p-4 shadow-md shadow-indigo-100">
-      <Image
-      alt="Home"
-        src="/proxym it intership.jpg"
-        className="h-56 w-full rounded-md object-cover"
-        width={100}
-        height={100}
-      />
+    <div>
+      <a href="#" className="block rounded-lg p-4 shadow-md shadow-indigo-100">
+        <Image
+          alt="Home"
+          src="/proxym it intership.jpg"
+          className="h-56 w-full rounded-md object-cover"
+          width={1000}
+          height={1000}
+        />
 
-      <div className="mt-2">
-        <dl>
-          <div>
-            <dt className="sr-only">Price</dt>
+        <div className="mt-2">
+          <dl>
+            <div>
+              <dt className="sr-only">Price</dt>
 
-            <dd className="text-sm text-gray-500">$240,000</dd>
-          </div>
-
-          <div>
-            <dt className="sr-only">Address</dt>
-
-            <dd className="font-medium">123 Wallaby Avenue, Park Road</dd>
-          </div>
-        </dl>
-
-        <div className="mt-6 flex items-center gap-8 text-xs">
-          <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-5 text-blue-900"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
-              />
-            </svg>
-
-            <div className="mt-1.5 sm:mt-0">
-              <p className="text-gray-500">Dimensions</p>
-
-              <p className="font-medium">10 cm * 20 cm</p>
+              <dd className="text-sm text-gray-500">$240,000</dd>
             </div>
-          </div>
 
-          <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-5 text-blue-900"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 6h.008v.008H6V6Z"
-              />
-            </svg>
+            <div>
+              <dt className="sr-only">Address</dt>
 
-            <div className="mt-1.5 sm:mt-0">
-              <p className="text-gray-500">Category</p>
+              <dd className="font-medium">123 Wallaby Avenue, Park Road</dd>
+            </div>
+          </dl>
 
-              <p className="font-medium border rounded-full px-2 py-0.5 mt-0.5 bg-blue-100 text-blue-900">Living Room</p>
+          <div className="mt-6 flex items-center gap-8 text-xs">
+            <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-blue-900"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
+                />
+              </svg>
+
+              <div className="mt-1.5 sm:mt-0">
+                <p className="text-gray-500">Dimensions</p>
+
+                <p className="font-medium">10 cm * 20 cm</p>
+              </div>
+            </div>
+
+            <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-blue-900"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 6h.008v.008H6V6Z"
+                />
+              </svg>
+
+              <div className="mt-1.5 sm:mt-0">
+                <p className="text-gray-500">Category</p>
+
+                <p className="font-medium border rounded-full px-2 py-0.5 mt-0.5 bg-blue-100 text-blue-900">
+                  Living Room
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </div>
   );
 }
